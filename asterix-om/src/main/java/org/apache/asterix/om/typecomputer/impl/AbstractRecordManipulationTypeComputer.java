31cc30e971a7d3ca4a04877125ffb05fdea1456b
Heri Ramampiaro <heriram@gmail.com>
2016-01-09 12:10:23 +0100

    ASTERIXDB-1187, ASTERIXDB-1162 and ASTERIXDB-1252 fixes, plus new internal functions
    This change contains the following updates and fixes:
    - New deep-eqaulity function which also handles numeric equality,
    e.g., [2.0, 2, 1.0] == [2, 2, 1].
    - New internal functions for record manipulation, including add and remove fields from records
    - Fixes for ASTERIXDB-1187: The RecordMergeDescriptor does not fully support
    open record merge. Merging records with open fields results in "NullPointerException".
    - Fixes to address issue ASTERIXDB-1162: dealing with closed or partly closed record
    added to an open record field.
    - Fixes issue ASTERIXDB-1252: Problems with Autogenerated Key
    
    Change-Id: I3621ebdd71c7cd91b50d77a972ad863cea7fcbc2

---
+++ asterix-om/src/main/java/org/apache/asterix/om/typecomputer/impl/AbstractRecordManipulationTypeComputer.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.asterix.om.typecomputer.impl;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.apache.asterix.common.exceptions.AsterixException;
+import org.apache.asterix.om.base.AString;
+import org.apache.asterix.om.base.IAObject;
+import org.apache.asterix.om.constants.AsterixConstantValue;
+import org.apache.asterix.om.typecomputer.base.IResultTypeComputer;
+import org.apache.asterix.om.types.ARecordType;
+import org.apache.asterix.om.types.ATypeTag;
+import org.apache.asterix.om.types.IAType;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.mutable.Mutable;
+import org.apache.hyracks.algebricks.common.exceptions.AlgebricksException;
+import org.apache.hyracks.algebricks.core.algebra.base.ILogicalExpression;
+import org.apache.hyracks.algebricks.core.algebra.expressions.AbstractFunctionCallExpression;
+import org.apache.hyracks.algebricks.core.algebra.expressions.ConstantExpression;
+import org.apache.hyracks.algebricks.core.algebra.expressions.IVariableTypeEnvironment;
+import org.apache.hyracks.algebricks.core.algebra.metadata.IMetadataProvider;
+
+abstract public class AbstractRecordManipulationTypeComputer implements IResultTypeComputer {
+
+    public static IAType mergeNestedRecordType(IAType nestedRecordTypeLeft, IAType nestedRecordTypeRight) throws AlgebricksException,
+            AsterixException {
+        if (nestedRecordTypeLeft.getTypeTag() != ATypeTag.RECORD || nestedRecordTypeRight.getTypeTag() != ATypeTag.RECORD) {
+            throw new AlgebricksException("Expected to merge two (sub-)records, but at least one of the types is not "
+                    + "a record type.");
+        }
+
+        ARecordType resultType = (ARecordType) nestedRecordTypeLeft;
+        ARecordType nestedRecordTypeRightCopy = (ARecordType) nestedRecordTypeRight;
+
+        for (int i = 0; i < nestedRecordTypeRightCopy.getFieldTypes().length; i++) {
+            try {
+                int pos = resultType.getFieldIndex(nestedRecordTypeRightCopy.getFieldNames()[i]);
+                if (pos >= 0) {
+                    // If a sub-record do merge, else ignore and let the values decide what to do
+                    if (nestedRecordTypeRightCopy.getFieldTypes()[i].getTypeTag() == ATypeTag.RECORD) {
+                        IAType[] oldTypes = resultType.getFieldTypes();
+                        oldTypes[pos] = mergeNestedRecordType(nestedRecordTypeRightCopy.getFieldTypes()[i],
+                                resultType.getFieldTypes()[pos]);
+                        resultType = new ARecordType(resultType.getTypeName(), resultType.getFieldNames(), oldTypes,
+                                resultType.isOpen());
+                    }
+                } else {
+                    IAType[] combinedFieldTypes = ArrayUtils.addAll(resultType.getFieldTypes().clone(),
+                            nestedRecordTypeRightCopy.getFieldTypes()[i]);
+                    resultType = new ARecordType(resultType.getTypeName(), ArrayUtils.addAll(
+                            resultType.getFieldNames(), nestedRecordTypeRightCopy.getFieldNames()[i]), combinedFieldTypes,
+                            resultType.isOpen());
+                }
+
+            } catch (IOException | AsterixException e) {
+                throw new AlgebricksException(e);
+            }
+        }
+
+        return resultType;
+    }
+
+    protected void addAdditionalFields(List<String> resultFieldNames, List<IAType> resultFieldTypes,
+            List<String> additionalFieldNames, List<IAType> additionalFieldTypes) throws AlgebricksException {
+
+        for (int i = 0; i < additionalFieldNames.size(); i++) {
+            String fn = additionalFieldNames.get(i);
+            IAType ft = additionalFieldTypes.get(i);
+            int pos = Collections.binarySearch(resultFieldNames, fn);
+            if (pos >= 0) {
+                IAType rt = resultFieldTypes.get(pos);
+                if (rt.getTypeTag() != ft.getTypeTag()) {
+                    throw new AlgebricksException("Duplicate field " + fn + " encountered");
+                }
+                try {
+                    // Only need to merge records
+                    if (ft.getTypeTag() == ATypeTag.RECORD && rt.getTypeTag() == ATypeTag.RECORD) {
+                        resultFieldTypes.set(pos, mergeNestedRecordType(ft, rt));
+                    }
+                } catch (AsterixException e) {
+                    throw new AlgebricksException(e);
+                }
+
+            } else {
+                resultFieldNames.add(fn);
+                resultFieldTypes.add(ft);
+            }
+        }
+    }
+
+    protected List<String> getListFromExpression(ILogicalExpression expression) throws AlgebricksException {
+        AbstractFunctionCallExpression funcExp = (AbstractFunctionCallExpression) expression;
+        List<Mutable<ILogicalExpression>> args = funcExp.getArguments();
+
+        List<String> list = new ArrayList<>();
+        for (Mutable<ILogicalExpression> arg : args) {
+            // At this point all elements has to be a constant
+            // Input list has only one level of nesting (list of list or list of strings)
+            ConstantExpression ce = (ConstantExpression) arg.getValue();
+            if (!(ce.getValue() instanceof AsterixConstantValue)) {
+                throw new AlgebricksException("Expecting a list of strings and found " + ce.getValue() + " instead.");
+            }
+            IAObject item = ((AsterixConstantValue) ce.getValue()).getObject();
+            ATypeTag type = item.getType().getTypeTag();
+            if (type == ATypeTag.STRING) {
+                list.add(((AString) item).getStringValue());
+            } else {
+                throw new AlgebricksException(type + " is currently not supported. Please check your function call.");
+            }
+        }
+
+        return list;
+    }
+
+    abstract public IAType computeType(ILogicalExpression expression, IVariableTypeEnvironment env,
+            IMetadataProvider<?, ?> metadataProvider) throws AlgebricksException;
+
+}
